b62b9a21bae8494e8a9b2222c6b12300fe8fdc60
KeiichiHirobe <chalenge.akane@gmail.com>
2020-06-21 01:03:27 +0900

    depend on new module b and automatically c version of go.mod changes from v1.0.0 to v1.1.0 when go build

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/KeiichiHirobe/gomodule-example-a
 
 go 1.14
 
-require github.com/KeiichiHirobe/gomodule-example-c v1.0.0
+require (
+	github.com/KeiichiHirobe/gomodule-example-b v1.0.0
+	github.com/KeiichiHirobe/gomodule-example-c v1.1.0
+)
--- main.go
+++ main.go
@@ -3,9 +3,15 @@ package main
 import (
 	"fmt"
 
+	b "github.com/KeiichiHirobe/gomodule-example-b"
 	c "github.com/KeiichiHirobe/gomodule-example-c"
 )
 
 func main() {
+	fmt.Println(c.GetVersionConfirm())
+	fmt.Println(b.GetThroughB())
+	fmt.Println(b.GetDecolatedThroughB())
+	b.SetThroughB("update by module b")
+	fmt.Println(b.GetThroughB())
 	fmt.Println(c.GetVersionConfirm())
 }
