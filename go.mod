5cc072cc4ad755b7f1934725d199a23f9d4478bf
KeiichiHirobe <chalenge.akane@gmail.com>
2020-06-21 03:54:57 +0900

    Revert "change version of go.mod from v1.1.0 to v2.0.0. go mod tidy never delete record of v1.1.0"
    This reverts commit 7c339d3ef959db62fd1947d7d2be18c0509a068a.

--- go.mod
+++ go.mod
@@ -4,5 +4,5 @@ go 1.14
 
 require (
 	github.com/KeiichiHirobe/gomodule-example-b v1.0.0
-	github.com/KeiichiHirobe/gomodule-example-c/v2 v2.0.0
+	github.com/KeiichiHirobe/gomodule-example-c v1.1.0
 )
--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 
 	b "github.com/KeiichiHirobe/gomodule-example-b"
-	c "github.com/KeiichiHirobe/gomodule-example-c/v2"
+	c "github.com/KeiichiHirobe/gomodule-example-c"
 )
 
 func main() {
